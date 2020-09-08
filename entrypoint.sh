8f7f125d370a354fe3b9f022e1c21fc26122a67e
Rémi <remi.bocquet@stadline.com>
2020-09-08 15:03:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 bzt -v ./../../taurus/config:/bzt-configs \
--v ./../../taurus/artifacts:/tmp/artifacts
+-v ./../../taurus/artifacts:/tmp/artifacts \
  -o execution.0.concurrency=${THREADS} \
 -o execution.0.ramp-up=${RAMPUP} \
 -o execution.0.hold-for=${HOLD} \
